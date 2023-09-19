5457ed0ddb75443adbb9d74ced9dd72ffa254e93
AskatAsh <abdulaskat@gmail.com>
2023-09-20 02:05:54 +0600

    used basic math operations

---
+++ Basics/basic-maths.js
@@ -0,0 +1,17 @@
+var x = 123;
+var y = 11;
+// console.log(x+y);
+var sum = x+y;
+console.log(sum);
+
+var sub = x-y;
+console.log(sub);
+
+var product = x*y;
+console.log(product);
+
+var div = y/x;
+console.log(div);
+
+var rem = y%x;
+console.log(rem);
