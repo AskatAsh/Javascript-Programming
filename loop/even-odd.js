5ff16cbabe53b7053bcbf42cdf608450169370e0
AskatAsh <abdulaskat@gmail.com>
2023-09-20 20:55:43 +0600

    used while loop to find even odd from 1 to n

---
+++ loop/even-odd.js
@@ -0,0 +1,17 @@
+var num = 10;
+var x = 1;
+
+// odd
+console.log("odd numbers from 1 to "+num+":");
+while(x<=10){
+    console.log(x);
+    x += 2;
+}
+
+// even
+console.log("even numbers from 1 to "+num+":");
+x = 2;
+while(x<=10){
+    console.log(x);
+    x += 2;
+}
