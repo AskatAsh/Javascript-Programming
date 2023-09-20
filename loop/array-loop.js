99b6d49c15b50440af7e1acf2b85145b62ac32ac
AskatAsh <abdulaskat@gmail.com>
2023-09-20 22:03:39 +0600

    used for loop to find sum of an array

--- loop/array-loop.js
+++ loop/array-loop.js
@@ -1,5 +1,12 @@
 var arr = [3, 45, 78, 10, 92, 23, 12];
+console.log(arr);
 
 for(var i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
+
+sum = 0;
+for(var i=0;i<arr.length;i++){
+    sum = sum + arr[i];
+}
+console.log("sum of the array is: "+ sum);
