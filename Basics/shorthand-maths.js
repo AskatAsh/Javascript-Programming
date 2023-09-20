061ed0fdb6b95fef203705a5376468757459cc6e
AskatAsh <abdulaskat@gmail.com>
2023-09-20 12:14:16 +0600

    used maths shorthand operations

---
+++ Basics/shorthand-maths.js
@@ -0,0 +1,25 @@
+var price = 243;
+price = price + 25;
+console.log(price);
+
+price += 10;
+console.log(price);
+
+price -= 43;
+console.log(price);
+
+price /= 5;
+console.log(price);
+
+price *= 10;
+console.log(price);
+
+var age = 24;
+// age = age + 1;
+// age += 1;
+age++;
+console.log(age);
+
+age = 45;
+age--;
+console.log(age);
