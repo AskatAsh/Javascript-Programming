9bb84ea75ce3ab35d922396f6e72fccb9897204c
AskatAsh <abdulaskat@gmail.com>
2023-09-20 14:19:04 +0600

    used if else to find grade of score

---
+++ if-else/grades.js
@@ -0,0 +1,17 @@
+var marks = 57;
+
+if(marks >= 80){
+    console.log("A grade");
+}
+else if(marks < 80 && marks >= 60){
+    console.log("B grade");
+}
+else if(marks < 60 && marks >= 50){
+    console.log("C grade");
+}
+else if(marks < 50 && marks >= 40){
+    console.log("C grade");
+}
+else{
+    console.log("F grade");
+}
