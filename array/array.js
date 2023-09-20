c147b69aea7029b015c2a073ba3eeb1d94ba96b7
AskatAsh <abdulaskat@gmail.com>
2023-09-20 17:25:13 +0600

    used basic array syntax index length

---
+++ array/array.js
@@ -0,0 +1,11 @@
+var num = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(num);
+
+var random = num[3];
+console.log(random);
+
+console.log(typeof(num));
+
+console.log(num.indexOf(50));
+
+console.log(num.length);
