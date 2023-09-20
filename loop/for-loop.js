92ddf824dcd40ac50ca8d3bd69f58525c7ed6d9d
AskatAsh <abdulaskat@gmail.com>
2023-09-20 21:02:12 +0600

    used for loop to find even odd

--- loop/for-loop.js
+++ loop/for-loop.js
@@ -1,5 +1,15 @@
 var n = 10;
 
-for(var i=1;i<=10;i++){
+// for(var i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// even
+for(var i = 2;i<=10;i+=2){
+    console.log(i);
+}
+
+// odd
+for(var i = 1;i<=10;i+=2){
     console.log(i);
 }
