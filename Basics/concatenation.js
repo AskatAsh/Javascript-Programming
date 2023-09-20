9dc4fb8ddcbec365b19edb056cb1e9444f06046c
AskatAsh <abdulaskat@gmail.com>
2023-09-20 12:22:47 +0600

    used basic string concatenation

---
+++ Basics/concatenation.js
@@ -0,0 +1,10 @@
+var firstName = "M.A.";
+var lastName = "Askat";
+
+var fullName = firstName + lastName;
+console.log(fullName);
+
+console.log(firstName + ' ' + lastName);
+
+var age = 24;
+console.log("Age: "+age);
