99cd58352d0a8b6dfac4422d5b0985796b85e770
AskatAsh <abdulaskat@gmail.com>
2023-09-20 12:57:27 +0600

    used prompt for taking input in javascript

---
+++ Basics/input.js
@@ -0,0 +1,11 @@
+// Gets user input
+var yourName = prompt("What's is your name?");
+var num = prompt("What is your favorite number? ");
+
+// Uses user input to print out information
+println("Hello " + yourName + "!");
+println(num + "?! That's my favorite number too!");
+
+// Prints out the variable type
+println("Name is a " + typeof yourName);
+println("Num is a " + typeof num);
