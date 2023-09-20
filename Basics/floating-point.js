01e328acb8e097f41fc80fc3392d0728f1bca4e1
AskatAsh <abdulaskat@gmail.com>
2023-09-20 12:38:22 +0600

    used to fixed for floating point numbers

---
+++ Basics/floating-point.js
@@ -0,0 +1,12 @@
+var num1 = 23.56789;
+var num2 = 12.456;
+var result = num1 * num2;
+console.log(result);
+console.log(typeof(result));
+
+var result = result.toFixed(2);
+console.log(result);
+console.log(typeof(result));
+
+console.log(parseFloat(result));
+console.log(typeof(parseFloat(result)));
