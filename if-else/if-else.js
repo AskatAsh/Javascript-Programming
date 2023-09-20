9077a99633a7b057963b57153a78d067a14b98aa
AskatAsh <abdulaskat@gmail.com>
2023-09-20 14:13:56 +0600

    used if else to find age period

--- if-else/if-else.js
+++ if-else/if-else.js
@@ -1,8 +1,14 @@
-var age = 34;
+var age = 14;
 
 if(age>18){
     console.log("Adulthood");
 }
 else{
     console.log("Childhood");
+    if(age<13){
+        console.log("Child");
+    }
+    else{
+        console.log("Teen");
+    }
 }
