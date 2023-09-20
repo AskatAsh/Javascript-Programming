ca9112747dfdc0f1f1f76e9fdd2e4b28ff10e00a
AskatAsh <abdulaskat@gmail.com>
2023-09-20 20:50:33 +0600

    used while loop to get sum of n numbers

--- loop/while-loop.js
+++ loop/while-loop.js
@@ -1,6 +1,13 @@
 var n = 10;
 var a = 1;
+// while(a<=n){
+//     console.log(a);
+//     a += 1;
+// }
+var sum = 0;
 while(a<=n){
     console.log(a);
+    sum = sum + a;
     a += 1;
 }
+console.log(sum);
