1e92709630cb337d91ec9e88091f9f3c5e02c6ab
AskatAsh <abdulaskat@gmail.com>
2023-09-20 17:41:42 +0600

    used push pop to add or remove elements

---
+++ array/push-pop.js
@@ -0,0 +1,16 @@
+var salad = ['tomato', 'onion', 'cucumber', 'carrot'];
+
+console.log(salad);
+
+salad.push('olive oil');
+console.log(salad);
+
+salad.push('salt');
+console.log(salad);
+
+salad.pop();
+console.log(salad);
+
+salad.pop()
+salad.pop()
+console.log(salad);
