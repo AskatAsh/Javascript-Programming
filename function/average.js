6aef43fcf453991162bcaea0e15b6d8e5ba4f02a
AskatAsh <abdulaskat@gmail.com>
2023-09-22 20:40:17 +0600

    used return statement in the average funtion

--- function/average.js
+++ function/average.js
@@ -1,6 +1,9 @@
 function average(num1, num2){
     let add = num1 + num2;
     let avg = add / 2;
-    console.log("average: "+avg);
+    // console.log("average: "+avg);
+    return avg;
 }
-average(23, 45);
+// average(23, 45);
+const result = average(23, 45);
+console.log("Average: "+result);
