007a6d470b637275cfc1ab16ad8474e30f95bccb
AskatAsh <abdulaskat@gmail.com>
2023-09-23 23:26:00 +0600

    created function to find factorial using while loop

--- problems/factorial.js
+++ problems/factorial.js
@@ -6,6 +6,20 @@ function factoUsingFor(num){
     return facto;
 }
 
-let myNumber = 5;
-let factorial = factoUsingFor(myNumber);
+// let myNumber = 5;
+// let factorial = factoUsingFor(myNumber);
+// console.log(factorial);
+
+function factoUsingWhile(num){
+    let facto = 1;
+    let i=1;
+    while(i<=num){
+        facto = facto * i;
+        i++;
+    }
+    return facto;
+}
+
+let myNumber = 4;
+let factorial = factoUsingWhile(myNumber);
 console.log(factorial);
