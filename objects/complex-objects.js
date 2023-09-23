3ea28e117fb9775a3249d96cd844500aeccb3b11
AskatAsh <abdulaskat@gmail.com>
2023-09-23 16:29:42 +0600

    created complex array and object

---
+++ objects/complex-objects.js
@@ -0,0 +1,27 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Jimmy James",
+      "title": "Today is Raining",
+      "release_year": 1998,
+      "formats": [
+        "mp3",
+        "mp4",
+        "CD"
+      ],
+    }
+  ];
+//   console.log(myMusic);
+//   console.log(myMusic[0]);
+//   console.log(myMusic[0].formats);
+  console.log(myMusic[0].formats[1]);
