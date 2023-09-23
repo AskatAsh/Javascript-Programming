ff5e5b41d656f6e06201c9429f95143ea66ee5b0
AskatAsh <abdulaskat@gmail.com>
2023-09-23 23:57:42 +0600

    stored odd elements in another array

--- problems/arrayOddSum.js
+++ problems/arrayOddSum.js
@@ -1,14 +1,20 @@
 function arrayOddSum(arr){
     let sum = 0;
+    const oddElements = [];
     for(let i=0;i<arr.length;i++){
         if(arr[i]%2 !== 0){
             sum = sum + arr[i];
             console.log(i,arr[i],sum);
+            oddElements.push(arr[i]);
         }
     }
-    return sum;
+    console.log("odd sum of the array: "+ sum);
+    return oddElements;
 }
 let myArray = [15, 22, 33, 15, 29, 10, 17];
 
-let result = arrayOddSum(myArray);
-console.log("odd sum of the array: "+result);
+// let result = arrayOddSum(myArray);
+// console.log("odd sum of the array: "+result);
+
+const oddElements = arrayOddSum(myArray);
+console.log(oddElements);
