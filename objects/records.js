f61b9f09449a44b2ea147d30316678a86d1808a3
AskatAsh <abdulaskat@gmail.com>
2023-09-23 18:54:35 +0600

    tested updateRecords function with arguments

--- objects/records.js
+++ objects/records.js
@@ -33,6 +33,10 @@ const recordCollection = {
       }
       return records;
   }
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-
-  console.log(recordCollection);
+// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+// updateRecords(recordCollection, 2548, "artist", "");
+// updateRecords(recordCollection, 2468, "tracks", "Free");
+// updateRecords(recordCollection, 2548, "tracks", "");
+updateRecords(recordCollection, 1245, "albumTitle", "Riptide");
+console.log(recordCollection);
