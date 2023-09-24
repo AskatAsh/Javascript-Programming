305658dde0c1e3ae0f673afcb612bce69796e834
AskatAsh <abdulaskat@gmail.com>
2023-09-24 22:55:59 +0600

    max in two numbers using ternary operator

--- problems/ternary.js
+++ problems/ternary.js
@@ -2,5 +2,11 @@ function checkEqual(a, b) {
     let ans = a ===b ? "Equal": "Not Equal";
     return ans;
 }
+console.log(checkEqual(3, 3));
 
-console.log(checkEqual(2, 2));
+function checkMax(a, b) {
+    let ans = a > b ? "max is: "+a : "max is: "+b;
+    return ans;
+}
+
+console.log(checkMax(3, 5));
