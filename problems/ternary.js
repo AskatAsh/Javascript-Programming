92e88a9c138b03966a536001ef4149eb5d8078cd
AskatAsh <abdulaskat@gmail.com>
2023-09-24 23:00:01 +0600

    max min equal in one function using ternary

--- problems/ternary.js
+++ problems/ternary.js
@@ -8,5 +8,11 @@ function checkMax(a, b) {
     let ans = a > b ? "max is: "+a : "max is: "+b;
     return ans;
 }
-
 console.log(checkMax(3, 5));
+
+function maxMinEqual(a, b) {
+    let ans = a === b ? "Equal" : a > b ? "max: "+a+" min: "+b : "max: "+b+" min: "+a;
+    return ans;
+}
+
+console.log(maxMinEqual(33, 13));
