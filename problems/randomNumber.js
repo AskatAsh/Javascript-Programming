ca3b0d1a4ba66120157bbb0990d2f177a1392a19
AskatAsh <abdulaskat@gmail.com>
2023-09-24 21:42:39 +0600

    modified to see the process

--- problems/randomNumber.js
+++ problems/randomNumber.js
@@ -1,6 +1,10 @@
 function randomWholeNum() {
-    return Math.floor(Math.random() * 10 + 1);
+    // return Math.floor(Math.random() * 10 + 1);
     // random number from 1 to 10;
+    let random = Math.random();
+    console.log(random);
+    // return Math.floor(random*10) + 1;
+    return Math.floor(random*10 + 1);
 }
 
 let myNum = randomWholeNum();
