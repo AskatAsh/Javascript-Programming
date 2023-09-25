47a440d7445608444d87b8545bbb49e6c0b415a4
AskatAsh <abdulaskat@gmail.com>
2023-09-25 20:39:12 +0600

    used string split method three different ways

---
+++ string/splitString.js
@@ -0,0 +1,9 @@
+let myString = "Jao Pakhi bolo tare. She jeno vole na moree. shukhe theko valo theko. Mone rekho ei Amare!!!";
+
+let splitBySpace = myString.split(' '); // converts string to array by removing space.
+let splitByDot = myString.split('. ');
+let splitByletter = myString.split('');
+
+console.log(splitBySpace);
+console.log(splitByDot);
+console.log(splitByletter);
