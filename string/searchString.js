16997029abb6d02f2db0ffb934ed297fcdac9867
AskatAsh <abdulaskat@gmail.com>
2023-09-25 20:28:30 +0600

    used starts and endswith method

--- string/searchString.js
+++ string/searchString.js
@@ -17,3 +17,18 @@ let myString = "Jao Pakhi bolo tare She jeno vole na moree shukhe theko valo the
 let find = "pakhi";
 let searchResult = searchString(myString, find);
 console.log(searchResult);
+
+function startEnd(myString, theStartWord, theEndWord){
+    let strLower = myString.toLowerCase();
+    let startWordLower = theStartWord.toLowerCase();
+    let endWordLower = theEndWord.toLowerCase();
+    let stardedWith = strLower.startsWith(startWordLower);
+    let endedWith = strLower.endsWith(endWordLower);
+    let result1 = "started with " +theStartWord+ " = "+ stardedWith;
+    let result2 = "ended with " +theEndWord+ " = "+ endedWith;
+    return result1 +"\n"+ result2;
+}
+
+let startWord = 'jao';
+let endword = 'amare';
+console.log(startEnd(myString, startWord, endword));
