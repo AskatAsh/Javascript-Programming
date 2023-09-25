6523d923988877a754c8c9de6daa7ba21b38e7f8
AskatAsh <abdulaskat@gmail.com>
2023-09-25 22:04:51 +0600

    used string slice method different ways

---
+++ string/sliceString.js
@@ -0,0 +1,12 @@
+let myString = "The quick brown fox jumps over the lazy dog.";
+
+let afterSlice_1 = myString.slice(20);
+let afterSlice_2 = myString.slice(4, 9);
+
+let afterSlice_3 = myString.slice(-4);
+let afterSlice_4 = myString.slice(-18, -14);
+
+console.log(afterSlice_1);
+console.log(afterSlice_2);
+console.log(afterSlice_3);
+console.log(afterSlice_4);
