b55ba4921c865cfb7306776faabd897fe66db001
AskatAsh <abdulaskat@gmail.com>
2023-09-25 22:20:19 +0600

    used charAt method to find char in specific index

---
+++ string/charAt.js
@@ -0,0 +1,14 @@
+let myString = "The quick brown fox jumps over the lazy dog.";
+
+let myChar_1 = myString.charAt(0);
+let myChar_2 = myString.charAt(5);
+let myChar_3 = myString.charAt(14);
+let myChar_4 = myString.charAt(30);
+let myChar_5 = myString.charAt(60);
+
+
+console.log("The character at index 0 is: " + "\'" + myChar_1 + "\'");
+console.log("The character at index 5 is: " + "\'" + myChar_2 + "\'");
+console.log("The character at index 14 is: " + "\'" + myChar_3 + "\'");
+console.log("The character at index 30 is: " + "\'" + myChar_4 + "\'");
+console.log("The character at index 60 is: " + "\'" + myChar_5 + "\'");
