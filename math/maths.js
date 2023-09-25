f298c97e6175e5e7f6d3e0f14517ccb7cd5d2aa4
AskatAsh <abdulaskat@gmail.com>
2023-09-25 23:49:43 +0600

    used abs and pow of Math method

---
+++ math/maths.js
@@ -0,0 +1,17 @@
+function absolute(num1, num2){
+    let result = Math.abs(num1 - num2);
+    return result;
+}
+
+// let absOfNum = absolute(12, 45);
+let absOfNum = absolute(12.62521, 44.1281823);
+console.log(absOfNum);
+
+function power(num1, num2){
+    let result = Math.pow(num1, num2);
+    return result;
+}
+
+// let powOfNum = power(4, 2);
+let powOfNum = power(2, -2);
+console.log(powOfNum);
