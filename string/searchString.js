46e5ff93619758d1294b5a86aa3b5fccba4ce952
AskatAsh <abdulaskat@gmail.com>
2023-09-25 15:43:30 +0600

    used toLowerCase method to convert string and word

--- string/searchString.js
+++ string/searchString.js
@@ -1,9 +1,11 @@
 function searchString(str, word){
-    let doesExist = str.includes(word);
+    let lowerString = str.toLowerCase();
+    let lowerWord = word.toLowerCase();
+    let doesExist = lowerString.includes(lowerWord);
     return doesExist;
 }
 
 let myString = "Jao Pakhi bolo tare She jeno vole na moree shukhe theko valo theko Mone rekho ei Amare!!!";
-let find = "More";
+let find = "pakhi";
 let searchResult = searchString(myString, find);
 console.log(searchResult);
