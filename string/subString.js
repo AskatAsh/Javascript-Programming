40dd1998feb755c36c539c673cf9eb6670c2d08a
AskatAsh <abdulaskat@gmail.com>
2023-09-25 22:11:06 +0600

    used subString method

---
+++ string/subString.js
@@ -0,0 +1,12 @@
+let myString = "The quick brown fox jumps over the lazy dog.";
+
+let subString_1 = myString.substring(31);
+let subString_2 = myString.substring(4, 9);
+
+let subString_3 = myString.substring(14, -4);
+let subString_4 = myString.substring(1, 0);
+
+console.log(subString_1);
+console.log(subString_2);
+console.log(subString_3);
+console.log(subString_4);
