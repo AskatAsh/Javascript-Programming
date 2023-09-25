4d87e00bc4163b0836717a950b8a4e68d67f63ba
AskatAsh <abdulaskat@gmail.com>
2023-09-25 15:51:09 +0600

    used indexOf method to find the word location

--- string/searchString.js
+++ string/searchString.js
@@ -1,8 +1,16 @@
-function searchString(str, word){
+function searchString(str, word) {
     let lowerString = str.toLowerCase();
     let lowerWord = word.toLowerCase();
     let doesExist = lowerString.includes(lowerWord);
-    return doesExist;
+    let location = lowerString.indexOf(lowerWord);
+    // return doesExist;
+    if (doesExist) {
+        let existsAt = "Yess..! Found " + word + " at index no: " + location;
+        return existsAt;
+    }
+    else{
+        return "Sorry..! couldn't find the word";
+    }
 }
 
 let myString = "Jao Pakhi bolo tare She jeno vole na moree shukhe theko valo theko Mone rekho ei Amare!!!";
