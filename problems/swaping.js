1910d2ed02baab09ad7a7ba5b7f28a3e19cec762
AskatAsh <abdulaskat@gmail.com>
2023-09-26 00:39:53 +0600

    swaping using array destructing method

--- problems/swaping.js
+++ problems/swaping.js
@@ -11,3 +11,15 @@ function swaping(num1, num2){
 }
 
 swaping(23, 45);
+
+function swapingAgain(first, second){
+    let beforeSwap = "before swap: ";
+    console.log(beforeSwap, first, second);
+
+    [first, second] = [second, first];
+    
+    let afterSwap = "after swap: ";
+    console.log(afterSwap, first, second);
+}
+
+swapingAgain(89, 46);
