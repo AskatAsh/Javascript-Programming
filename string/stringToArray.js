1ecfe29c1a8351b7307c7173253e096a04909683
AskatAsh <abdulaskat@gmail.com>
2023-09-25 15:04:29 +0600

    replaced space elements with underscore

--- string/stringToArray.js
+++ string/stringToArray.js
@@ -4,7 +4,11 @@ function stringToArray(str){
         // arr.unshift(str[i]);
         arr.push(str[i]);
     }
-    
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === ' '){
+            arr[i] = '_';
+        }
+    }
     return arr;
 }
 
