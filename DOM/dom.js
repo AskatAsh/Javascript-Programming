fe6d347fa2bb8fb74d6ab382aaddc534940f69e6
AskatAsh <abdulaskat@gmail.com>
2023-09-26 23:08:05 +0600

    getting elements by class and tag names

---
+++ DOM/dom.js
@@ -0,0 +1,20 @@
+// console.log('hello this is dom.js file');
+// console.log(document);
+// console.log(document.doctype);
+
+let lists = document.getElementsByTagName('li');
+for (const li of lists){
+    // console.log(li.textContent);
+    console.log(li.innerText);
+}
+
+let headings = document.getElementsByTagName('h1');
+for (const h1 of headings){
+    console.log(h1);
+    console.log(h1.innerText);
+}
+
+let classList = document.getElementsByClassName('lists');
+for (const list of classList){
+    console.log(list.innerText);
+}
