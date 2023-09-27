e159e25a72e8b337492f03f45b80ac5e3da9e4ef
AskatAsh <abdulaskat@gmail.com>
2023-09-27 23:24:34 +0600

    added event click on close button

--- Dom-Events/script.js
+++ Dom-Events/script.js
@@ -1,8 +1,8 @@
-function makeLightBlue(){
+function makeLightBlue() {
     document.body.style.backgroundColor = 'lightblue';
 }
 
-function makeLightGreen(){
+function makeLightGreen() {
     document.body.style.backgroundColor = 'lightgreen';
 }
 
@@ -10,7 +10,7 @@ const clickToChange = document.getElementById('makeLightGreen').onclick = makeLi
 
 const pinkButton = document.getElementById('makePink');
 // console.log(pinkButton);
-pinkButton.onclick = function makePink(){
+pinkButton.onclick = function makePink() {
     document.body.style.backgroundColor = 'pink';
 }
 
@@ -22,7 +22,7 @@ console.log(greenButton);
 const greenText = document.getElementById('addText');
 console.log(greenText);
 
-function addGreenText(){
+function addGreenText() {
     greenText.innerText = 'Green after clicked';
     greenText.style.color = 'green';
     // greenText.style.backgroundColor = 'lightgreen';
@@ -39,7 +39,7 @@ greenButton.addEventListener('click', addGreenText);
 const bgButton = document.getElementById('bgChange');
 // console.log(bgButton);
 
-bgButton.addEventListener('click', function changeBackground(){
+bgButton.addEventListener('click', function changeBackground() {
     bgButton.style.backgroundColor = 'darkblue';
     bgButton.style.color = 'white';
     bgButton.style.padding = '5px 10px';
@@ -50,19 +50,36 @@ bgButton.addEventListener('click', function changeBackground(){
 const seeDetail = document.getElementById('detailsButton');
 // console.log(seeDetail);
 
+const detailsContainer = document.getElementById('modalContainer');
+function detailsContainerStyle() {
+    detailsContainer.style.width = '100%';
+    detailsContainer.style.height = '100%';
+    detailsContainer.style.position = 'absolute';
+    detailsContainer.style.top = '0';
+    detailsContainer.style.left = '0';
+    detailsContainer.style.display = 'flex';
+    detailsContainer.style.alignItems = 'center';
+    detailsContainer.style.justifyContent = 'center';
+    detailsContainer.style.backgroundColor = 'rgba(0,0,255,0.2)';
+    detailsContainer.style.zIndex = '99';
+}
+
 const detailsSection = document.getElementById('details');
 // console.log(detailsSection.childNodes[5].innerText);
-// detailsSection.style.display = 'none';
+detailsSection.style.display = 'none';
 
-function styleDetails(){
-    detailsSection.style.zIndex = '99';
+function styleDetails() {
+    detailsSection.style.position = 'absolute';
+    detailsSection.style.boxShadow = '0px 2px 8px rgba(0,0,0,0.5)';
+    detailsSection.style.display = 'block';
+    // detailsSection.style.zIndex = '99';
     detailsSection.style.backgroundColor = 'darkblue';
     detailsSection.style.borderRadius = '8px';
     detailsSection.style.textAlign = 'center';
-    detailsSection.style.width = '300px';
+    detailsSection.style.width = '20rem';
     detailsSection.style.color = 'white';
     detailsSection.style.padding = '50px';
-    detailsSection.style.margin = '0 auto';
+    // detailsSection.style.margin = '0 auto';
     detailsSection.childNodes[5].style.padding = '12px 20px';
     detailsSection.childNodes[5].style.border = 'none';
     detailsSection.childNodes[5].style.borderRadius = '8px';
@@ -70,7 +87,15 @@ function styleDetails(){
     detailsSection.childNodes[5].style.color = 'blue';
     detailsSection.childNodes[5].style.fontWeight = 'bold';
     detailsSection.childNodes[5].style.marginTop = '10px';
-    document.body.style.backgroundColor = 'rgba(0,0,0,0.8)'
+    // document.body.style.backgroundColor = 'rgba(0,0,0,0.8)'
+    detailsContainerStyle();
+}
+
+const closeButton = document.getElementById('close');
+function close() {
+    detailsContainer.style.display = 'none';
 }
 
 seeDetail.addEventListener('click', styleDetails);
+
+closeButton.addEventListener('click', close);
