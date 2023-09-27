a6565c7f7b5f7b1e647723d863d80f70dc46a1b3
AskatAsh <abdulaskat@gmail.com>
2023-09-27 22:26:33 +0600

    added text using addEventListener

--- Dom-Events/script.js
+++ Dom-Events/script.js
@@ -13,3 +13,25 @@ const pinkButton = document.getElementById('makePink');
 pinkButton.onclick = function makePink(){
     document.body.style.backgroundColor = 'pink';
 }
+
+// javascript add event listener
+
+const greenButton = document.getElementById('makeGreen');
+console.log(greenButton);
+
+const greenText = document.getElementById('addText');
+console.log(greenText);
+
+function addGreenText(){
+    greenText.innerText = 'Green after clicked';
+    greenText.style.color = 'green';
+    // greenText.style.backgroundColor = 'lightgreen';
+    greenText.style.border = '2px solid green';
+    greenText.style.width = '150px';
+    greenText.style.textAlign = 'center';
+    greenText.style.padding = '20px';
+
+}
+
+// greenButton.onclick = addGreenText;
+greenButton.addEventListener('click', addGreenText);
