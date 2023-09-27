b5ad45e512657851e78125f41bea7dc1b1e3e9c1
AskatAsh <abdulaskat@gmail.com>
2023-09-27 20:00:57 +0600

    connected id to a javascript function by onclick

--- Dom-Events/script.js
+++ Dom-Events/script.js
@@ -1,3 +1,9 @@
 function makeLightBlue(){
     document.body.style.backgroundColor = 'lightblue';
 }
+
+function makeLightGreen(){
+    document.body.style.backgroundColor = 'lightgreen';
+}
+
+const clickToChange = document.getElementById('makeLightGreen').onclick = makeLightGreen;
