012da89e30b8c2eee50823d351098ad6db8a4913
AskatAsh <abdulaskat@gmail.com>
2023-09-27 22:58:37 +0600

    created details modal using addEventlistener

--- Dom-Events/script.js
+++ Dom-Events/script.js
@@ -35,3 +35,42 @@ function addGreenText(){
 
 // greenButton.onclick = addGreenText;
 greenButton.addEventListener('click', addGreenText);
+
+const bgButton = document.getElementById('bgChange');
+// console.log(bgButton);
+
+bgButton.addEventListener('click', function changeBackground(){
+    bgButton.style.backgroundColor = 'darkblue';
+    bgButton.style.color = 'white';
+    bgButton.style.padding = '5px 10px';
+    bgButton.innerText = 'Thank You!';
+});
+
+// details modal
+const seeDetail = document.getElementById('detailsButton');
+// console.log(seeDetail);
+
+const detailsSection = document.getElementById('details');
+// console.log(detailsSection.childNodes[5].innerText);
+// detailsSection.style.display = 'none';
+
+function styleDetails(){
+    detailsSection.style.zIndex = '99';
+    detailsSection.style.backgroundColor = 'darkblue';
+    detailsSection.style.borderRadius = '8px';
+    detailsSection.style.textAlign = 'center';
+    detailsSection.style.width = '300px';
+    detailsSection.style.color = 'white';
+    detailsSection.style.padding = '50px';
+    detailsSection.style.margin = '0 auto';
+    detailsSection.childNodes[5].style.padding = '12px 20px';
+    detailsSection.childNodes[5].style.border = 'none';
+    detailsSection.childNodes[5].style.borderRadius = '8px';
+    detailsSection.childNodes[5].style.backgroundColor = 'white';
+    detailsSection.childNodes[5].style.color = 'blue';
+    detailsSection.childNodes[5].style.fontWeight = 'bold';
+    detailsSection.childNodes[5].style.marginTop = '10px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.8)'
+}
+
+seeDetail.addEventListener('click', styleDetails);
