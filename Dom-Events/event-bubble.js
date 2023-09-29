0c1a2a388d443b6b45579407f575ec1c947fedf4
AskatAsh <abdulaskat@gmail.com>
2023-09-29 13:01:33 +0600

    created event bubble effect using function

---
+++ Dom-Events/event-bubble.js
@@ -0,0 +1,15 @@
+document.getElementById('item-2').addEventListener('click', function(){
+    console.log('item-2 is Clicked!');
+});
+
+document.getElementById('myList').addEventListener('click', function(){
+    console.log('ul myList is Clicked!');
+});
+
+document.getElementById('listContainer').addEventListener('click', function(){
+    console.log('list container is Clicked!');
+});
+
+document.getElementById('theBody').addEventListener('click', function(){
+    console.log('the body is Clicked!');
+});
