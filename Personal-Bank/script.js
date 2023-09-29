a6cb7faa98aecf4edc6eec1e002eeb30dd3c97aa
AskatAsh <abdulaskat@gmail.com>
2023-09-29 18:38:33 +0600

    got user email using click event

---
+++ Personal-Bank/script.js
@@ -0,0 +1,7 @@
+const loginBtn = document.getElementById('loginButton');
+// console.log(loginBtn);
+loginBtn.addEventListener('click', function(event){
+    const userEmail = document.getElementById('userEmail');
+    // console.log(userEmail.value);
+    const email = userEmail.value;
+});
