bcd6d9ef729f8ef4d484e655bc6a73df20dbbf28
AskatAsh <abdulaskat@gmail.com>
2023-09-29 13:43:49 +0600

    used stopPropagation to stop event bubble

--- Dom-Events/event-bubble.js
+++ Dom-Events/event-bubble.js
@@ -1,9 +1,11 @@
-document.getElementById('item-2').addEventListener('click', function(){
+document.getElementById('item-2').addEventListener('click', function(event){
     console.log('item-2 is Clicked!');
+    event.stopPropagation();
 });
 
-document.getElementById('myList').addEventListener('click', function(){
+document.getElementById('myList').addEventListener('click', function(event){
     console.log('ul myList is Clicked!');
+    event.stopImmediatePropagation();
 });
 
 document.getElementById('listContainer').addEventListener('click', function(){
