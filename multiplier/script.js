2479d7e4386631b4f04d62cb5fcbebb41170fc04
AskatAsh <abdulaskat@gmail.com>
2023-10-01 21:21:02 +0600

    commented the prompt method lines

--- multiplier/script.js
+++ multiplier/script.js
@@ -1,5 +1,5 @@
-const key = prompt('Write the key you want to set');
-const value = prompt('Write the value you want to set');
+// const key = prompt('Write the key you want to set');
+// const value = prompt('Write the value you want to set');
 
 // set key and value to local storage
 localStorage.setItem(key, value);
