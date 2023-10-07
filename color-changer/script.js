e6d3459bf65ef51037cb41164310792ee6ce06c0
AskatAsh <abdulaskat@gmail.com>
2023-10-08 00:33:55 +0600

    changed id and used event.target.id for color

--- color-changer/script.js
+++ color-changer/script.js
@@ -7,17 +7,21 @@ buttons.forEach(function(button){
         // console.log(event);
         // console.log(event.target);
         // body.style.backgroundColor = 'green';
-        if(event.target.id === "lightGray"){
-            body.style.backgroundColor = 'lightgray';
+        if(event.target.id === "lightgray"){
+            // body.style.backgroundColor = 'lightgray';
+            body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id === "offWhite"){
-            body.style.backgroundColor = 'wheat';
+        if(event.target.id === "wheat"){
+            // body.style.backgroundColor = 'wheat';
+            body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id === "lightBlue"){
-            body.style.backgroundColor = 'lightblue';
+        if(event.target.id === "lightblue"){
+            // body.style.backgroundColor = 'lightblue';
+            body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id === "lightGreen"){
-            body.style.backgroundColor = 'lightgreen';
+        if(event.target.id === "lightgreen"){
+            // body.style.backgroundColor = 'lightgreen';
+            body.style.backgroundColor = event.target.id;
         }
     })
 });
