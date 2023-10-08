737b19426c6d522dc0939a4a92511995e92b65f7
AskatAsh <abdulaskat@gmail.com>
2023-10-09 01:12:17 +0600

    used Object.keys method to get object parameters

---
+++ Intermediate/keys.js
@@ -0,0 +1,10 @@
+const laptop = {
+    name: "HP Elitebook",
+    color: "silver",
+    price: 35000,
+    isSecondHand: true
+}
+// console.log(laptop);
+// console.log(Object.keys(laptop));
+const keys = Object.keys(laptop);
+console.log(keys);
