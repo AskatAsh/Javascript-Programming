1d718a6c239ccac15f155c795d29ba69c437b018
AskatAsh <abdulaskat@gmail.com>
2023-10-08 21:59:09 +0600

    used spread operator to get max number from an array

---
+++ Intermediate/spread-operator.js
@@ -0,0 +1,8 @@
+const maxNum = Math.max(12, 34, 44, 29, 92, 73, 57);
+console.log(maxNum);
+
+// using spread operator
+const numArray = [30, 40, 50, 83, 98, 23];
+// console.log(...numArray);
+const myMax = Math.max(...numArray);
+console.log(myMax);
