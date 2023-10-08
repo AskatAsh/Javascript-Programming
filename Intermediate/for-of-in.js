6cfc72d7672a158aa3e9b4c576ea07b1d9a708bd
AskatAsh <abdulaskat@gmail.com>
2023-10-09 01:40:53 +0600

    used for in method in object

--- Intermediate/for-of-in.js
+++ Intermediate/for-of-in.js
@@ -1,11 +1,23 @@
 const numbers = [2, 4, 6, 8, 10, 12];
 // for(let i=0;i<numbers.length;i++){}
 for(const num of numbers){
-    console.log(num);
+    // console.log(num);
 }
 
 const myString = "Hello Bangladesh";
 for(const char of myString){
-    console.log(char);
+    // console.log(char);
 }
 // for of cannnot be used in object
+
+const laptop = {
+    name: "HP Elitebook",
+    color: "silver",
+    price: 35000,
+    isSecondHand: true
+}
+
+for(const key in laptop){
+    const value = laptop[key];
+    console.log(key, value);
+}
