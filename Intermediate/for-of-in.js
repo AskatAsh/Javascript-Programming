f2701ae69dc88ebf738fd6cc2e9eb0d029511150
AskatAsh <abdulaskat@gmail.com>
2023-10-09 01:36:39 +0600

    used for of method in array and string

---
+++ Intermediate/for-of-in.js
@@ -0,0 +1,11 @@
+const numbers = [2, 4, 6, 8, 10, 12];
+// for(let i=0;i<numbers.length;i++){}
+for(const num of numbers){
+    console.log(num);
+}
+
+const myString = "Hello Bangladesh";
+for(const char of myString){
+    console.log(char);
+}
+// for of cannnot be used in object
