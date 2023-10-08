4d1377398a847833d91fbd6c60ed473e6a2b7f79
AskatAsh <abdulaskat@gmail.com>
2023-10-09 01:51:42 +0600

    print single string in multiple line using backtick

---
+++ Intermediate/multi-line.js
@@ -0,0 +1,6 @@
+// const string = "I am a Developer. I love to code. I love to travel";
+// console.log(string);
+const string = `I am a Developer.
+I love to code.
+I love to travel`;
+console.log(string);
