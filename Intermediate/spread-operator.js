e7398cb4a6b7f03ce679f057602b8e96726230e6
AskatAsh <abdulaskat@gmail.com>
2023-10-08 22:25:26 +0600

    stopped array referencing using spread operator

--- Intermediate/spread-operator.js
+++ Intermediate/spread-operator.js
@@ -6,3 +6,21 @@ const numArray = [30, 40, 50, 83, 98, 23];
 // console.log(...numArray);
 const myMax = Math.max(...numArray);
 console.log(myMax);
+
+// array to array reference
+const myArray = [10, 20, 30, 40, 50, 60];
+const copyArray = myArray;
+copyArray.push(70);
+
+// console.log(`original array = ${myArray}`);
+// console.log(`copy array = ${copyArray}`);
+console.log("original array = ", myArray);
+console.log("copy array = ", copyArray);
+
+// stop array referencing using spread operator
+
+const mySecondArray = [10, 20, 30, 40, 50, 60, 70];
+const secondCopy = [...mySecondArray];
+secondCopy.push(80);
+console.log("original array = ", mySecondArray);
+console.log("copy array = ", secondCopy);
