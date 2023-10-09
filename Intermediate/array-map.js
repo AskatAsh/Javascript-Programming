c7ba0d78f9610b639a6fd3efb1e13df2ae255c66
AskatAsh <abdulaskat@gmail.com>
2023-10-10 01:24:06 +0600

    used map in array containing string

--- Intermediate/array-map.js
+++ Intermediate/array-map.js
@@ -18,4 +18,10 @@ const finalResult = arr1.map(n => n*2);
 const numbers = [1, 3, 5, 7, 9, 11];
 
 const square = numbers.map(n => n*n);
-console.log(square);
+// console.log(square);
+
+const members = ['asif', 'rakib', 'shihab', 'rahim', 'hannan'];
+const nameLength = members.map(member => member.length);
+// console.log(nameLength);
+const firstLetter = members.map(first => first[0]);
+console.log(firstLetter);
