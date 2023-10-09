7aed2bcbf1c219a188ab51428d4476134a0aa20a
AskatAsh <abdulaskat@gmail.com>
2023-10-10 01:17:06 +0600

    got square of numbers using one line arrow function in map

--- Intermediate/array-map.js
+++ Intermediate/array-map.js
@@ -13,4 +13,9 @@ const output = arr1.map(doubleNum2);
 // console.log(output);
 
 const finalResult = arr1.map(n => n*2);
-console.log(finalResult);
+// console.log(finalResult);
+
+const numbers = [1, 3, 5, 7, 9, 11];
+
+const square = numbers.map(n => n*n);
+console.log(square);
