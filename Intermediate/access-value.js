89089276d88f55111f0414cd413bd7040be81fa5
AskatAsh <abdulaskat@gmail.com>
2023-10-09 22:11:00 +0600

    accessing object inside array inside object

--- Intermediate/access-value.js
+++ Intermediate/access-value.js
@@ -1,3 +1,13 @@
 const data = [{id: 369, name: "Bulbul", age: 45, address: "Mollar Bari"}];
 
-console.log(data[0].address);
+// console.log(data[0].address);
+
+const products = {
+    count: 4500,
+    data: [
+        {id: 1, name: "lenovo", price: 25000, stock: 43},
+        {id: 2, name: "dell", price: 34000, stock: 23}
+    ]
+}
+
+console.log(products.data[1].price);
