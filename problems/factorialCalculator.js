47c881da91fd753595bb14e36a9956965481fa36
AskatAsh <abdulaskat@gmail.com>
2023-10-10 18:04:37 +0600

    solved factorial using for loop

--- problems/factorialCalculator.js
+++ problems/factorialCalculator.js
@@ -1,8 +1,16 @@
+// function factorial(n) {
+//     if(n==0){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+
 function factorial(n) {
-    if(n==0){
-        return 1;
+    let fact = 1;
+    for(let i=1;i<=n;i++){
+        fact = fact * i;
     }
-    return n*factorial(n-1);
+    return fact;
 }
 
 console.log(factorial(5));
