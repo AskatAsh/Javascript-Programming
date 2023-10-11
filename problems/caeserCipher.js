89865751f083ac7322951f02f7acaaded84e5e7c
AskatAsh <abdulaskat@gmail.com>
2023-10-11 13:58:04 +0600

    solved the problem caesar cipher

---
+++ problems/caeserCipher.js
@@ -0,0 +1,17 @@
+function caesarCipher(str, shift) {
+    let encrypted = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') {
+            encrypted += ' ';
+        }
+        else if (str.charCodeAt(i) + shift > 122) {
+            encrypted += String.fromCharCode(str.charCodeAt(i) + shift - 122 + 96);
+        }
+        else {
+            encrypted += String.fromCharCode(str.charCodeAt(i) + shift);
+        }
+    }
+    return encrypted;
+}
+console.log(caesarCipher("xyz", 5));
+// Expected output: "cde"
