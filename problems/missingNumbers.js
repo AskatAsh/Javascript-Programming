80b25cf7cbc75c4795520b332a6f06394faddaeb
AskatAsh <abdulaskat@gmail.com>
2023-10-11 12:47:50 +0600

    solved missing numbers problem in a different way

--- problems/missingNumbers.js
+++ problems/missingNumbers.js
@@ -1,13 +1,25 @@
+// function findMissingNumbers(arr, start, end) {
+//     let missing = [];
+//     for(let i = start; i<=end; i++){
+//         let found = 0;
+//         for(let j=0; j<arr.length; j++){
+//             if(i === arr[j]){
+//                 found++;
+//             }
+//         }
+//         if(found === 0) missing.push(i);
+//     }
+//     return missing;
+// }
+
 function findMissingNumbers(arr, start, end) {
     let missing = [];
+    let newSet = new Set(arr);
+
     for(let i = start; i<=end; i++){
-        let found = 0;
-        for(let j=0; j<arr.length; j++){
-            if(i === arr[j]){
-                found++;
-            }
+        if(!newSet.has(i)){
+            missing.push(i);
         }
-        if(found === 0) missing.push(i);
     }
     return missing;
 }
