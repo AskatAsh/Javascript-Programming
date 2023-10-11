96b7475bc9d459d6ace75c9c0afa97a461adeeaa
AskatAsh <abdulaskat@gmail.com>
2023-10-12 00:43:08 +0600

    got letter more than 5 in array using filter

---
+++ problems solved/stringFilter.js
@@ -0,0 +1,3 @@
+const words = ['the','overall', 'level', 'of', 'javascript', 'keeps', 'increasing'];
+const moreThanFiveLetters = words.filter(word => word.length > 5);
+console.log(moreThanFiveLetters);
