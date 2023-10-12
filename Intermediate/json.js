27156fa89af423bc189dbceb48c6274563fad5ac
AskatAsh <abdulaskat@gmail.com>
2023-10-12 17:46:47 +0600

    convert string to object using JSON.parse

--- Intermediate/json.js
+++ Intermediate/json.js
@@ -5,3 +5,9 @@ const object = {id: 345, name: 'Askat', job: 'web developer'};
 const stringified = JSON.stringify(object);
 
 console.log(stringified);
+console.log(typeof stringified);
+
+const objectified = JSON.parse(stringified);
+
+console.log(objectified);
+console.log(typeof objectified);
