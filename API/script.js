530ba420522f1bead2e4b5475ee5fff526e5d087
AskatAsh <abdulaskat@gmail.com>
2023-10-12 22:48:40 +0600

    get username from user data api

--- API/script.js
+++ API/script.js
@@ -16,6 +16,6 @@ function loadUserData(){
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then(data => data.forEach(user => {
-            console.log(user)
+            console.log(user.username)
         }))
 }
