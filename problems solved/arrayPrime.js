41b9d0259be164a476e8449edc47e2a91b05db5a
AskatAsh <abdulaskat@gmail.com>
2023-10-12 13:59:36 +0600

    made the code more efficient

--- problems solved/arrayPrime.js
+++ problems solved/arrayPrime.js
@@ -1,12 +1,11 @@
 const numbers = [2, 5, 10, 13, 17];
 const primes = numbers.filter(num => {
-    for(let i=2;i<=Math.sqrt(num) || num ===2;i++){
-        if(num%i !== 0 || num === 2){
-            return num;
-        }
-        else{
-            break;
+    if(num < 2) return false;
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num%i === 0){
+            return false;
         }
     }
+    return true;
 });
 console.log(primes);
