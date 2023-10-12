e34e26481410cece8765d51068ee20b92d043826
AskatAsh <abdulaskat@gmail.com>
2023-10-12 14:38:53 +0600

    used some method to filter uppercase letter words

--- problems solved/getUppercase.js
+++ problems solved/getUppercase.js
@@ -2,13 +2,15 @@
 // const words = ['a', 'b', 'c', 'D', 'E'];
 const words = ['UPPERCASE', 'lowercase', 'MiXeD', 'cApItAlS'];
 
-const onlyUpperCase = words.filter(word => {
-    for(const letter of word){
-        if(letter>='A' && letter<='Z'){
-            return true;
-        }
-    }
-    return false;
-});
+// const onlyUpperCase = words.filter(word => {
+//     for(const letter of word){
+//         if(letter>='A' && letter<='Z'){
+//             return true;
+//         }
+//     }
+//     return false;
+// });
+
+const onlyUpperCase = words.filter(word => word.split('').some(letter => letter>='A' && letter<='Z'));
 
 console.log(onlyUpperCase);
