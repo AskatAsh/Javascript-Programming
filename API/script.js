93742f6227015f69ff9b9a6700d72fe3527d5d41
AskatAsh <abdulaskat@gmail.com>
2023-10-12 22:46:08 +0600

    get user data using fetch and for each

--- API/script.js
+++ API/script.js
@@ -11,3 +11,11 @@ function loadData(){
         .then(response => response.json())
         .then(json => console.log(json))
 }
+
+function loadUserData(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => data.forEach(user => {
+            console.log(user)
+        }))
+}
