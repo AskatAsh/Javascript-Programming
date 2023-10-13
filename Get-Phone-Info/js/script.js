c2072e0a3b246c132de18ffb2f19df3e4d11f6bb
AskatAsh <abdulaskat@gmail.com>
2023-10-13 23:10:57 +0600

    used async await to get phone info from an API

--- Get-Phone-Info/js/script.js
+++ Get-Phone-Info/js/script.js
@@ -1,8 +1,16 @@
-
-// load phone info using fetch api
-const loadPhoneInfo = () => {
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(data => data.json())
-        .then(json => console.log(json.data))
+const loadPhoneInfo = async () =>{
+    const phoneData = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const phoneInfo = await phoneData.json();
+    console.log(phoneInfo.data);
 }
 loadPhoneInfo();
+
+
+
+// // load phone info using fetch api
+// const loadPhoneInfo = () => {
+//     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+//         .then(data => data.json())
+//         .then(json => console.log(json.data))
+// }
+// loadPhoneInfo();
