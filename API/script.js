2a4dab18784e63f01bd130c7cb43bf5c16809897
AskatAsh <abdulaskat@gmail.com>
2023-10-13 15:43:32 +0600

    uncommented some codes to fetch api

--- API/script.js
+++ API/script.js
@@ -2,27 +2,27 @@
 //       .then(response => response.json())
 //       .then(json => console.log(json))
 
-// const url = "https://jsonplaceholder.typicode.com/todos/1";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
 // // fetch(url)
 // //     .then(response => response.json())
 // //     .then(json => console.log(json))
-// function loadData(){
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(json => console.log(json))
-// }
+function loadData(){
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
 
-// function loadUserData(){
-//     const ul = document.getElementById('usersList');
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//         .then(res => res.json())
-//         .then(data => data.forEach(user => {
-//             // console.log(user.username)
-//             const li = document.createElement('li');
-//             li.innerHTML = user.username
-//             ul.appendChild(li)
-//         }))
-// }
+function loadUserData(){
+    const ul = document.getElementById('usersList');
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => data.forEach(user => {
+            // console.log(user.username)
+            const li = document.createElement('li');
+            li.innerHTML = user.username
+            ul.appendChild(li)
+        }))
+}
 
 function loadPostsData(){
     const div = document.getElementById('postsData');
