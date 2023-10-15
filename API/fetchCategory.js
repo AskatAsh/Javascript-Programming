cfae63cc4459736572c6740aada5da596e289fcc
AskatAsh <abdulaskat@gmail.com>
2023-10-15 20:03:36 +0600

    showed category got from API to html

--- API/fetchCategory.js
+++ API/fetchCategory.js
@@ -2,6 +2,19 @@ const fetchCategory = async () => {
     const data = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const gotData = await data.json();
     const dataArray = gotData.data
-    console.log(dataArray);
+    // console.log(dataArray);
+    getPostDate(dataArray);
 }
 fetchCategory();    
+
+const getPostDate = (dataArray) => {
+    const categoryList = document.getElementById('showCategory');
+    dataArray.forEach(data => {
+        
+        // console.log(data.category);
+        category = data.category;
+        const li = document.createElement('li');
+        li.innerHTML = category;
+        categoryList.appendChild(li);
+    })
+}
