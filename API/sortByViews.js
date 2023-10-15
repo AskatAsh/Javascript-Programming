fae2cd53a24bab99dd8b2ec1aa8a7ed79f2a951f
AskatAsh <abdulaskat@gmail.com>
2023-10-15 21:43:26 +0600

    fetched data from API

---
+++ API/sortByViews.js
@@ -0,0 +1,7 @@
+const fetchCategory = async () => {
+    const data = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const gotData = await data.json();
+    const dataArray = gotData.data
+    console.log(dataArray);
+}
+fetchCategory();
