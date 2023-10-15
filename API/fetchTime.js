0152f212c758c7d8d9afd0f67869af66a2d63c56
AskatAsh <abdulaskat@gmail.com>
2023-10-15 19:42:45 +0600

    created function to convert to hour and minute

--- API/fetchTime.js
+++ API/fetchTime.js
@@ -18,8 +18,26 @@ const getPostDate = (dataArray) => {
             timeList.appendChild(li);
         }
         else{
-            li.innerHTML = postDate;
+            const convertedTime = toHourAndMinutes(parseInt(postDate));
+            li.innerHTML = convertedTime;
             timeList.appendChild(li);
         }     
     })
+}
+
+const toHourAndMinutes = (second) => {
+    if(second >= 3600){
+        const toHour = Math.floor(second/3600);
+        const rem = second%3600;
+        const toMinute = Math.floor(rem/60); 
+ 
+        return `${toHour}h ${toMinute}m ago`;
+    }
+    else if(second >= 60 && second <= 3600){
+        const toMinute = Math.floor(second/60);
+        return `${toMinute}m ago`;
+    }
+    else{
+        return `${second}s ago`;
+    }
 }
