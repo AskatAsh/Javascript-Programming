5bd854d305c3ab4c904e808670bd8e018570a4d0
AskatAsh <abdulaskat@gmail.com>
2023-10-15 21:11:24 +0600

    get category id from API using forEach

--- API/fetchCategory.js
+++ API/fetchCategory.js
@@ -3,18 +3,31 @@ const fetchCategory = async () => {
     const gotData = await data.json();
     const dataArray = gotData.data
     // console.log(dataArray);
+    getCategoryId(dataArray);
     getPostDate(dataArray);
 }
-fetchCategory();    
+fetchCategory();
 
+const categoryList = document.getElementById('showCategory');
 const getPostDate = (dataArray) => {
-    const categoryList = document.getElementById('showCategory');
-    dataArray.forEach(data => {
-        
+    dataArray.forEach(data => {        
         // console.log(data.category);
         category = data.category;
         const li = document.createElement('li');
         li.innerHTML = category;
         categoryList.appendChild(li);
+    })
+}
+
+categoryList.addEventListener('click', function(event){
+    console.log(event.target.innerHTML);
+    // getCategory(event.target.innerHTML)
+})
+
+
+const getCategoryId = (dataArray) => {
+    dataArray.forEach(data => {        
+        const categoryId = data.category_id;
+        console.log(categoryId);
     })
 }
