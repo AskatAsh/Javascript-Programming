f0809ced970cbd302747090ea1cb23e9f9b88506
AskatAsh <abdulaskat@gmail.com>
2023-10-15 22:31:43 +0600

    added the views in an array using push method

--- API/sortByViews.js
+++ API/sortByViews.js
@@ -9,9 +9,21 @@ fetchCategory();
 
 const getViews = (dataArray) => {
     // const timeList = document.getElementById('showTime');
+    const viewsArray = [];
     dataArray.forEach(data => {
         const views = data.others.views;
-        console.log(views);
+        // console.log(views);
+        const viewsInNum = removeLetterFromString(views);
+        console.log(viewsInNum);
+        viewsArray.push(viewsInNum);
         // const li = document.createElement('li');
     })
+    console.log(viewsArray);
+}
+
+const removeLetterFromString = (str) => {
+    // const newString = str.slice(0, -1);
+    const newString = str.substring(0, str.length-1);
+    const toNum = parseFloat(newString);
+    return toNum;
 }
