4564ff229c810ba31685a89af96faa6bcb58dad9
AskatAsh <abdulaskat@gmail.com>
2023-10-17 16:45:22 +0600

    used queryselector to select elements

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -1,4 +1,11 @@
 // console.log( Math.floor(Math.random()*100)+1 );
 // const randomNumber = parseInt(Math.random()*100+1);
 const randomNumber = Math.floor(Math.random()*100)+1 ;
-console.log(randomNumber);
+// console.log(randomNumber);
+
+const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#numInput');
+const numOfGuesses = document.querySelector('.guesses');
+const guessesRemaining = document.querySelector('.remaining');
+const lowOrHigh = document.querySelector('.lowOrHigh');
+console.log(lowOrHigh);
