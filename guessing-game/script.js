1030561add4c0cc974fb1a36b742ccf3e6b65a5c
AskatAsh <abdulaskat@gmail.com>
2023-10-17 16:35:57 +0600

    used two different ways to generate random number from 1to100

---
+++ guessing-game/script.js
@@ -0,0 +1,4 @@
+// console.log( Math.floor(Math.random()*100)+1 );
+// const randomNumber = parseInt(Math.random()*100+1);
+const randomNumber = Math.floor(Math.random()*100)+1 ;
+console.log(randomNumber);
