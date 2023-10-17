e97b1c60a1a2aac41fa63a69cd4edc0c13264dde
AskatAsh <abdulaskat@gmail.com>
2023-10-17 17:03:17 +0600

    created neccessary functions for guessing game

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -12,3 +12,31 @@ const starOver = document.querySelector('#results');
 console.log(starOver);
 
 const p = document.createElement('p');
+
+let prevGuess = [];
+let guessCount = 1;
+let playGame = true;
+
+function validateGuess(guess) {
+    //validate input values of the input field
+}
+
+function checkGuess(guess){
+    // checks if the guessed value is correct
+}
+
+function displayGuess(guess){
+    // displays previous guesses and remaining guessse
+}
+
+function displayMessage(message){
+    // displays low or high message
+}
+
+function newGame(){
+    // starts the game
+}
+
+function endGame(){
+    // ends the game
+}
