de98da78df167ec16de8e34244ddd62a54199a99
AskatAsh <abdulaskat@gmail.com>
2023-10-19 20:42:52 +0600

    used window dom to get keycode on keydown

---
+++ keypress/script.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class="color">
+            <table border="1" cellspacing="0" cellpadding="5px">
+                <tr>
+                    <th>Key</th>
+                    <th>Keycode</th>
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? "Space" : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+                <tr>
+            </table>
+        </div>
+    `
+});
