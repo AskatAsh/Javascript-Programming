b131d7a64e04f95f37664b53e0c3f0aefda6072f
AskatAsh <abdulaskat@gmail.com>
2023-10-25 23:08:17 +0600

    used this method in function inside object

--- objects/this.js
+++ objects/this.js
@@ -2,8 +2,19 @@ const book = {
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
     pages: 180,
-    bookDetails(){
+    bookDetails() {
         return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`;
     }
 }
 console.log(book.bookDetails());
+
+const book2 = {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    pages: 281,
+    bookDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`;
+    }
+}
+
+console.log(book2.bookDetails());
