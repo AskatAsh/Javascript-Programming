8b2046d76839e565bf9071c33c46f2af528cbad2
AskatAsh <abdulaskat@gmail.com>
2023-10-26 00:06:00 +0600

    used !isNaN to get sum of values in object

--- objects/sumNumberValues.js
+++ objects/sumNumberValues.js
@@ -1,13 +1,26 @@
+// function sumNumberValues(obj) {
+//     const values = Object.values(obj);
+//     // return typeof(values[0]);
+//     let sum = 0;
+//     for(let val of values){
+//         if(typeof(val) === 'number'){
+//             sum += val;
+//         }
+//     }
+//     return sum;
+// }
+
 function sumNumberValues(obj) {
     const values = Object.values(obj);
-    // return typeof(values[0]);
+    
     let sum = 0;
     for(let val of values){
-        if(typeof(val) === 'number'){
+        if( !isNaN(parseFloat(val)) ){
             sum += val;
         }
     }
     return sum;
+    // return isNaN(values[2]);
 }
 
 const data = {
