e36e76481e82683f59fe20faf8c36fe397c04196
AskatAsh <abdulaskat@gmail.com>
2023-10-26 23:42:26 +0600

    pass object as parameter in resolve

--- promise/createPromise.js
+++ promise/createPromise.js
@@ -8,3 +8,22 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log("promise called or consumed");
 })
+
+const promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log("Async task 2");
+        resolve();
+    }, 1000);
+}).then(function(){
+    console.log("Async task 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({user: "Askat", email: "askat@example.com"});
+    }, 1000);
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
