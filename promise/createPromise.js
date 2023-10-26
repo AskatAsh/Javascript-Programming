4fb5ab0b5c705e0c7977aaa77b744bf5744c4de1
AskatAsh <abdulaskat@gmail.com>
2023-10-27 01:43:41 +0600

    error handling using trycatch in async await

--- promise/createPromise.js
+++ promise/createPromise.js
@@ -1,70 +1,74 @@
-const promiseOne = new Promise(function(resolve, reject){
+const promiseOne = new Promise(function (resolve, reject) {
     setTimeout(() => {
         console.log("This is inside the created promise");
         resolve();
     }, 1000);
 })
 
-promiseOne.then(function(){
+promiseOne.then(function () {
     console.log("promise called or consumed");
 })
 
-const promiseTwo = new Promise(function(resolve, reject){
+const promiseTwo = new Promise(function (resolve, reject) {
     setTimeout(() => {
         console.log("Async task 2");
         resolve();
     }, 1000);
-}).then(function(){
+}).then(function () {
     console.log("Async task 2 resolved");
 })
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({user: "Askat", email: "askat@example.com"});
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ user: "Askat", email: "askat@example.com" });
     }, 1000);
 })
 
-promiseThree.then(function(user){
+promiseThree.then(function (user) {
     console.log(user);
 })
 
-const promiseFour = new Promise(function(resolve, reject){
+const promiseFour = new Promise(function (resolve, reject) {
     setTimeout(() => {
         const error = false;
-        if(!error){
-            resolve({username: "Askat", password: "123"});
-        } else{
+        if (!error) {
+            resolve({ username: "Askat", password: "123" });
+        } else {
             reject("Error: Something went wrong!");
         }
     }, 1000);
 })
 
 promiseFour
-.then((user) => {
-    console.log(user);
-    return user.username;
-})
-.then((username) => {
-    console.log(username);
-})
-.catch((error) => {
-    console.log(error);
-})
-.finally(() => console.log("The promise is either resolved or rejected."));
+    .then((user) => {
+        console.log(user);
+        return user.username;
+    })
+    .then((username) => {
+        console.log(username);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => console.log("The promise is either resolved or rejected."));
 
-const promiseFive = new Promise(function(resolve, reject){
+const promiseFive = new Promise(function (resolve, reject) {
     setTimeout(() => {
         const error = true;
-        if(!error){
-            resolve({language: "JavaScript", version: "ES6"});
-        } else{
+        if (!error) {
+            resolve({ language: "JavaScript", version: "ES6" });
+        } else {
             reject("Error: JS went wrong!");
         }
     }, 1000);
 });
 
-async function consumePromiseFive(){
-    const response = await promiseFive;
-    console.log(response);
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 consumePromiseFive();
