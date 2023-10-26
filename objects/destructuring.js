e1511b567871a291c72e026165b7c833413705df
AskatAsh <abdulaskat@gmail.com>
2023-10-26 13:03:04 +0600

    used object destructuring method

---
+++ objects/destructuring.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'Alice',
+    age: 20,
+    grade: 'A',
+    country: 'USA',
+  };
+  
+const {name, age, grade, country} = student;
+  
+  console.log(name);   // Expected Output: 'Alice'
+  console.log(age);    // Expected Output: 20
+  console.log(grade);  // Expected Output: 'A'
+  console.log(country); // Expected Output: 'USA'
+  
