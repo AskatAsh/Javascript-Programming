efa67b13404021f27ce832bcb2e2bbe114d4df78
AskatAsh <abdulaskat@gmail.com>
2024-07-08 22:04:18 +0600

    Added changes and error debugged for extension

--- Mini Projects/Conversions/pxToRem/pxToRem.js
+++ Mini Projects/Conversions/pxToRem/pxToRem.js
@@ -1,82 +1,76 @@
-const inputField = document.querySelector('#inputField');
-const outputField = document.querySelector('#ouputField');
-const convertBtn = document.querySelector('#convertBtn');
+const inputField = document.getElementById("inputField");
+const outputField = document.getElementById("outputField");
+const convertBtn = document.querySelector("#convertBtn");
 
-const fromText = document.querySelector('.from');
-const toText = document.querySelector('.to');
+const fromText = document.querySelector(".from");
+const toText = document.querySelector(".to");
 
-const selectedPxtorem = document.querySelector('#pxtorem');
-const selectedRemtopx = document.querySelector('#remtopx');
+const selectedPxtorem = document.querySelector("#pxtorem");
+const selectedRemtopx = document.querySelector("#remtopx");
 
-const headerText = document.querySelector('.headerText');
-const errorText = document.querySelector('.error');
+const headerText = document.querySelector(".headerText");
+const errorText = document.querySelector(".error");
 
-let pxOrrem = 'pxtorem';
-
-convertBtn.addEventListener('click', function () {
-
-    const inputValue = inputField.value;
-
-    if (isNaN(inputValue)) {
-        // alert("Please Enter a valid Number!");
-        errorText.classList.remove('hide');
-    }
-    else {
-        errorText.classList.add('hide');
-        // console.log(inputValue);
-        if (pxOrrem == 'remtopx') {
-            remtopx(inputValue);
-        }
-        else {
-            pxTorem(inputValue);
-        }
-    }
-
-    // inputField.value = '';
-    // console.log(pxOrrem);
-})
-
-// this function converts pixel to rem
-const pxTorem = (px) => {
+document.addEventListener("DOMContentLoaded", function () {
+  // this function converts pixel to rem
+  const pxTorem = (px) => {
     const toRem = px / 16;
     // console.log(toRem);
     outputField.innerHTML = toRem;
-}
+  };
 
-// this function converts pixel to rem
-const remtopx = (rem) => {
+  // this function converts rem to pixel
+  const remtopx = (rem) => {
     const toPx = rem * 16;
     const pxToInt = Math.ceil(rem * 16);
     const diff = pxToInt - toPx;
     if (diff < 1 && diff != 0) {
-        outputField.innerHTML = `${toPx} ~ ( ${pxToInt} )`;
+      outputField.innerHTML = `${toPx} ~ ( ${pxToInt} )`;
+    } else {
+      outputField.innerHTML = toPx;
     }
-    else {
-        outputField.innerHTML = toPx;
+    // console.log(toPx, pxToInt);
+  };
+
+  selectedRemtopx.addEventListener("click", (e) => {
+    e.target.classList.add("selected");
+    selectedPxtorem.classList.remove("selected");
+    // console.log("selected px to rem");
+    fromText.innerHTML = "rem";
+    toText.innerHTML = "pixel";
+    pxOrrem = "remtopx";
+    inputField.value = "";
+    outputField.textContent = "0";
+    headerText.innerText = "Rem to Pixel";
+  });
+
+  selectedPxtorem.addEventListener("click", (e) => {
+    e.target.classList.add("selected");
+    selectedRemtopx.classList.remove("selected");
+    // console.log("selected px to rem");
+    fromText.innerHTML = "pixel";
+    toText.innerHTML = "rem";
+    pxOrrem = "pxtorem";
+    inputField.value = "";
+    outputField.textContent = "0";
+    headerText.innerText = "Pixel to Rem";
+  });
+
+  let pxOrrem = "pxtorem";
+  convertBtn.addEventListener("click", function () {
+    const inputValue = inputField.value;
+
+    if (isNaN(inputValue) || inputValue == "") {
+      // alert("Please Enter a valid Number!");
+      errorText.classList.remove("hide");
+    } else {
+      errorText.classList.add("hide");
+      // console.log(inputValue);
+      if (pxOrrem == "remtopx") {
+        remtopx(inputValue);
+      } else {
+        pxTorem(inputValue);
+      }
     }
-    console.log(toPx, pxToInt);
-}
-
-selectedRemtopx.addEventListener('click', (e) => {
-    e.target.classList.add('selected');
-    selectedPxtorem.classList.remove('selected');
-    // console.log("selected px to rem");
-    fromText.innerHTML = 'rem';
-    toText.innerHTML = 'pixel';
-    pxOrrem = 'remtopx';
-    inputField.value = '';
-    outputField.innerText = '';
-    headerText.innerText = 'Rem to Pixel';
-})
-
-selectedPxtorem.addEventListener('click', (e) => {
-    e.target.classList.add('selected');
-    selectedRemtopx.classList.remove('selected');
-    // console.log("selected px to rem");
-    fromText.innerHTML = 'pixel';
-    toText.innerHTML = 'rem';
-    pxOrrem = 'pxtorem';
-    inputField.value = '';
-    outputField.innerText = '';
-    headerText.innerText = 'Pixel to Rem';
-})
+  });
+});
